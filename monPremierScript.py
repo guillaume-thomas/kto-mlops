90575de79118da245bd665c836390453f329fdf8
Thomas Lyautey <97942763+GuidePandore21@users.noreply.github.com>
2025-02-03 13:02:16 +0000

    Refactor : Réécriture du code avec l'implémentation de fonction

--- monPremierScript.py
+++ monPremierScript.py
@@ -1,14 +1,15 @@
+"""
+Count names with more than seven letters
+"""
+def names(prenoms):
+    more_than_seven = 0
+    for prenom in prenoms:
+        if len(prenom) > 7:
+            more_than_seven += 1
+            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+        else:
+            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
+    return more_than_seven
+
 prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-more_than_seven = 0
-for prenom in prenoms:
-    if len(prenom) > 7:
-        more_than_seven += 1
-        print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-    else:
-        print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(more_than_seven))
-
-def saluer(nom: str) -> str:
-    return "Bonjour " + nom
-
-print(saluer("Alice"))  # Affiche : Bonjour Alice
+print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(names(prenoms=prenoms)))
