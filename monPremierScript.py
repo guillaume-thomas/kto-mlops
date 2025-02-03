78f8b87fff3280f6386d72a8f216a5275a75f147
Thomas Lyautey <97942763+GuidePandore21@users.noreply.github.com>
2025-02-03 12:54:59 +0000

    Refactor : Compte les mots comptant plus de 7 lettres dans une liste

--- monPremierScript.py
+++ monPremierScript.py
@@ -1,7 +1,9 @@
-message = "C'est mon premier script !!!"
-print(message)
-
-je_change_de_type = 1
-print(type(je_change_de_type))
-je_change_de_type = "coucou"
-print(type(je_change_de_type))
+prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+more_than_seven = 0
+for prenom in prenoms:
+    if len(prenom) > 7:
+        more_than_seven += 1
+        print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+    else:
+        print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
+print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(more_than_seven))
